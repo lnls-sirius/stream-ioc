79afcf417daa9bbe43e33d2a568f38ea1f71b517
Aureo de Souza Carneiro Freitas <aureo.carneiro@lnls.br>
2018-03-16 16:59:12 -0300

    Adc Thermo files 16/03 17h

---
+++ iocBoot/RADThermo.cmd
@@ -0,0 +1,33 @@
+#!../bin/linux-arm/streamApp
+
+# RADThermo.cmd
+
+# This script is being used for the new ELSE Nuclear probes of LNLS Radiation Protection Group
+# (RAD).
+
+# Environment variables
+
+epicsEnvSet("EPICS_BASE", "/root/base-3.15.5")
+epicsEnvSet("ASYN", "/root/asyn4-33")
+epicsEnvSet("TOP", "/root/stream-ioc")
+epicsEnvSet("ARCH", "linux-arm")
+epicsEnvSet ("STREAM_PROTOCOL_PATH", "$(TOP)/protocol")
+
+# Database definition file
+
+cd ${TOP}
+dbLoadDatabase("dbd/streamApp.dbd")
+streamApp_registerRecordDeviceDriver(pdbbase)
+
+# Port for the ELSE probes
+
+drvAsynIPPortConfigure("IPPort1", "127.0.0.1:17002 UDP")
+
+# Records of the ELSE probes
+
+dbLoadRecords("database/THERMO-Probes.db", "PORT = IPPort1, PREFIX = RAD:THERMO")
+
+# Effectively initializes the IOC
+
+cd iocBoot
+iocInit
