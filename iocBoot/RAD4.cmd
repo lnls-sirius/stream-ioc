32963b26b2c1ab32891db2a8bc5a0fd2429293dc
root <root@beaglebone.localdomain>
2018-03-21 08:58:38 -0300

    Change IP UDP server RAD4

--- iocBoot/RAD4.cmd
+++ iocBoot/RAD4.cmd
@@ -1,6 +1,6 @@
 #!../bin/linux-arm/streamApp
 
-# RAD3.cmd
+# RAD4.cmd
 
 # This script is being used for the new ELSE Nuclear probes of LNLS Radiation Protection Group
 # (RAD).
@@ -21,7 +21,7 @@ streamApp_registerRecordDeviceDriver(pdbbase)
 
 # Port for the ELSE probes
 
-drvAsynIPPortConfigure("IPPort1", "127.0.0.1:17002 UDP")
+drvAsynIPPortConfigure("IPPort1", "127.0.0.1:17003 UDP")
 
 # Records of the ELSE probes
 
