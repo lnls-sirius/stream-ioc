9998fa9e5cceb226aea90aab15dcea7f4036232f
Aureo Freitas <aureo.carneiro@gmail.com>
2018-04-16 11:40:19 -0300

    Adc ELSE-SATURN5702 and Thermo-FHT6020 files to stream-ioc/interface

---
+++ interface/ELSE-SATURN5702.py
@@ -0,0 +1,263 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Módulo necessário
+
+import serial
+import requests
+import socket
+import sys
+import threading
+import time
+import datetime
+import logging
+
+# Definindo as viaveis
+
+sample = 14400
+dataBuffer = [0.0]*5
+timeBuffer = [0]*sample
+deltatimeBuffer = [0]*sample
+gammaBuffer = [0.0]*sample
+neutronBuffer = [0.0]*sample
+integralgamma = 0.0
+integralneutron = 0.0
+integral = 0.0
+dataGamma = 0.0
+dataNeutron = 0.0
+dataTotal = 0.0
+dataG = 0.0
+dataN = 0.0
+ipGamma = "192.168.0.100"
+ipNeutron = "192.168.0.200"
+timeref = datetime.datetime.utcnow()
+i = 0
+j = 0
+iterate = sample
+
+UDP_PORT = int(sys.argv[1])
+
+client_socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket1.connect((ipGamma, 10001))
+
+client_socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket2.connect((ipNeutron, 10001))
+
+# Log File for exceptions
+
+logging.basicConfig(filename='app.log',level=logging.INFO)
+
+# Função que inclui o checksum e o caractere <ETX> a qualquer mensagem que se deseja enviar
+
+def format_message(message):
+
+    checksum = ord(message[3]) ^ ord(message[4])
+    for character in message[5:]:
+        checksum ^= ord(character)
+    return (message + "{0:02X}".format(checksum) + "\x03")
+
+
+# Cria socket TCP/IP para comunicação com o SATURN I 5700 RTM da sonda de gamma
+
+def cnct1(ip):
+
+    # Requisita os valores das janelas de tempo para cáculo da média móvel
+    # COLOCAR TB O VALOR O THRESHOLD LEVEL 01001GL RESPOSTA: (STX)01024GL1234.56:1234.56:1234.56
+    # (CS)(CS) (ETX)
+
+    client_socket1.send(format_message("\x0201001MI"))
+    data = client_socket1.recv(1024)
+    if data.find("K") != -1 or data.find("P") != -1 or data.find("B") != -1 or data.find("A") != -1 or  data.find("E") != -1 or  data.find("D") != -1 or  data.find("C") != -1 or  data.find("H") != -1:
+        if data[9: len(data) - 3] != "" and data[0] == "\x02":
+            return float(data[9: 15])
+    else:
+        pass
+
+def cnct2(ip):
+
+    # Requisita os valores das janelas de tempo para cáculo da média móvel
+    # COLOCAR TB O VALOR O THRESHOLD LEVEL 01001GL RESPOSTA: (STX)01024GL1234.56:1234.56:1234.56
+    # (CS)(CS) (ETX)
+
+    client_socket2.send(format_message("\x0201001MI"))
+    data = client_socket2.recv(1024)
+    if data.find("K") != -1 or data.find("P") != -1 or data.find("B") != -1 or data.find("A") != -1 or data.find("E") != -1 or data.find("D") != -1 or data.find("C") != -1 or data.find("H") != -1:
+        if data[9: len(data) - 3] != "" and data[0] == "\x02":
+            return float(data[9: 15])
+    else:
+        pass
+
+        # Funcao para calcular o delta time para integral
+
+def time_sec(timeBuffer):
+
+    deltatime = (timeBuffer[-1] - timeBuffer[-2]).total_seconds()
+    return deltatime
+
+
+# Thread Principal
+
+def scanThread():
+
+    # Global Variables
+
+    global integral
+    global dataBuffer
+    global SERIAL_PORT
+    global dataGamma
+    global dataNeutron
+    global dataTotal
+    global dataN
+    global dataG
+    global timeBuffer
+    global deltatimeBuffer
+    global ipNeutron
+    global ipGamma
+    global client_socket1
+    global client_socket2
+    global integralneutron
+    global integralgamma
+    global gammaBuffer
+    global neutronBuffer
+    global i
+    global j
+    global timeref
+    global iterate
+
+    # Main Loop
+
+    while (True) :
+
+        try:
+            # Delay de 0.5s ============ Testar
+
+            time.sleep(1)
+
+            # Send the pct for ELSE SATURN
+            dataGamma = cnct1(ipGamma)
+
+            dataNeutron = cnct2(ipNeutron)
+
+            # If receiver void menssage do nothing
+
+            if dataNeutron != None and dataGamma != None:
+
+                dataTotal = dataGamma + dataNeutron
+                dataBuffer.append(dataTotal)
+                neutronBuffer.append(dataNeutron)
+                gammaBuffer.append(dataGamma)
+
+        # Save the time now in Buffer Time
+
+            timeBuffer.append(datetime.datetime.utcnow())
+
+        # If save just one time do:
+
+            if timeBuffer[-1] != 0 and timeBuffer[-2] != 0 and dataNeutron != None and dataGamma != None:
+
+                if (timeBuffer[-1] - timeref).total_seconds() > sample and (timeBuffer[-2] - timeref).total_seconds() < sample:
+
+                    integralneutron = 0
+                    integralgamma = 0
+                    integral = 0
+                    j=0
+
+                    while j <= i:
+
+                        integralgamma += ((gammaBuffer[j] + gammaBuffer[j+1]) * deltatimeBuffer[j]) / (2 * 3600)
+                        integralneutron += ((neutronBuffer[j] + neutronBuffer[j + 1]) * deltatimeBuffer[j]) / (2 * 3600)
+                        integral += ((dataBuffer[j] + dataBuffer[j + 1]) * deltatimeBuffer[j]) / (2 * 3600)
+                        j += 1
+
+                    iterate = i
+                    timeref = timeBuffer[-1]
+                    i=0
+
+                else:
+
+                    deltatime = time_sec(timeBuffer)
+                    deltatimeBuffer.append(deltatime)
+                    integralneutron += ((((neutronBuffer[-1] + neutronBuffer[-2]) * deltatime) - ((neutronBuffer[0] + neutronBuffer[1]) * deltatimeBuffer[0])) / (2 * 3600))
+                    integralgamma += ((((gammaBuffer[-1] + gammaBuffer[-2]) * deltatime) - ((gammaBuffer[0] + gammaBuffer[1]) * deltatimeBuffer[0])) / (2 * 3600))
+                    integral += ((((dataBuffer[-1] + dataBuffer[-2]) * deltatime) - ((dataBuffer[0] + dataBuffer[1]) * deltatimeBuffer[0])) / (2 * 3600))
+
+
+
+        # Fix the time of buffers only in sample time
+
+            if len(dataBuffer) > (iterate):
+                dataBuffer = dataBuffer[1:]
+
+            if len(timeBuffer) > 5:
+                timeBuffer = timeBuffer[1:]
+
+            if len(deltatimeBuffer) > iterate:
+                deltatimeBuffer = deltatimeBuffer[1:]
+
+            if len(gammaBuffer) > iterate:
+                gammaBuffer = gammaBuffer[1:]
+
+            if len(neutronBuffer) > iterate:
+                neutronBuffer = neutronBuffer[1:]
+
+        # Incrementa o contador i
+
+            i += 1
+
+        except Exception as e:
+            print(e)
+            logging.error("Error occurred" + str(e))
+            pass
+
+
+# Inicializa a Main Thread
+
+auxiliary_thread = threading.Thread(target = scanThread)
+auxiliary_thread.setDaemon(True)
+auxiliary_thread.start()
+
+# O programa em sua verção final irá dormir 500s para garantir a leitura certa da sonda
+
+udp_server_address = ("0.0.0.0", UDP_PORT)
+udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_server_socket.bind(udp_server_address)
+
+# Loop Principal
+
+while (True):
+
+    # Client (EPICS IOC) input data and address
+
+    data, address = udp_server_socket.recvfrom(512)
+
+    # There is a simple protocol for communication to the client
+
+    if (data):
+
+        if (data == "RAD_E_Gamma?\n" and dataNeutron != None and dataGamma != None):
+            answer = "{:.10f}".format(gammaBuffer[-1])
+
+        elif (data == "RAD_E_Neutron?\n" and dataNeutron != None and dataGamma != None):
+            answer = "{:.10f}".format(neutronBuffer[-1])
+
+        elif (data == "RAD_E_Total?\n" and dataNeutron != None and dataGamma != None):
+            answer = "{:.10f}".format(dataBuffer[-1])
+
+        elif (data == "RAD_E_Integral?\n" and dataNeutron != None and dataGamma != None):
+            answer = "{:.10f}".format(integral)
+
+        elif (data == "RAD_E_IntegralGamma?\n" and dataNeutron != None and dataGamma != None):
+            answer = "{:.10f}".format(integralgamma)
+
+        elif (data == "RAD_E_IntegralNeutron?\n" and dataNeutron != None and dataGamma != None):
+            answer = "{:.10f}".format(integralneutron)
+
+        else:
+            continue
+
+        answer += "\n"
+        udp_server_socket.sendto(answer, address)
+
+        # The end
+
+
---
+++ interface/Thermo-FHT6020.py
@@ -0,0 +1,262 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Importando as bibliotecas
+
+import serial
+import socket
+import sys
+import threading
+import datetime
+import logging
+
+# Definindo o pino do P9 14 como saída para o osciloscópio
+
+sample = 14400
+dataBuffer = [0.0]*5
+timeBuffer = [0]*sample
+deltatimeBuffer = [0]*sample
+gammaBuffer = [0.0]*sample
+neutronBuffer = [0.0]*sample
+integralgamma = 0.0
+integralneutron = 0.0
+integral = 0.0
+dataGamma = 0.0
+dataNeutron = 0.0
+dataTotal = 0.0
+dataG = 0.0
+dataN = 0.0
+deltatime = 0.0
+i=0
+j=0
+iterate = sample
+timeref = datetime.datetime.utcnow()
+
+SERIAL_PORT = str(sys.argv[1])
+
+UDP_PORT = int(sys.argv[2])
+
+# Log File for exceptions
+logging.basicConfig(filename='app.log',level=logging.INFO)
+
+# Funcao para incluir o checksum no pacote
+
+def incluirChecksum(entrada):
+    soma = 0
+    for elemento in entrada:
+        soma += ord(elemento)
+    soma = soma % 256
+    return(entrada + "{0:02X}".format(soma) + "\x03")
+
+# Funcao para calcular o delta time para integral
+
+def time_sec(timeBuffer):
+    deltatime = (timeBuffer[-1] - timeBuffer[-2]).total_seconds()
+    return deltatime
+
+# Thread Principal
+
+def scanThread():
+
+    # Global Variables
+
+    global integral
+    global dataBuffer
+    global SERIAL_PORT
+    global dataGamma
+    global dataNeutron
+    global dataTotal
+    global dataN
+    global dataG
+    global timeBuffer
+    global deltatimeBuffer
+    global integralgamma
+    global integralneutron
+    global gammaBuffer
+    global neutronBuffer
+    global sample
+    global deltatime
+    global i
+    global j
+    global timeref
+    global iterate
+
+    # Inicialização da interface serial
+
+    serial_interface = serial.Serial(port = "{}".format(SERIAL_PORT),
+                                     baudrate = 19200,
+                                     bytesize = serial.SEVENBITS,
+                                     parity = serial.PARITY_EVEN,
+                                     stopbits = serial.STOPBITS_TWO,
+                                     timeout = 0.5
+                                    )
+
+    # 01 Gamma ----------- 02 Neutron
+
+    while (True):
+
+        # Delay de 500ms para cara envio de pacote
+
+        #time.sleep(0.500)
+
+        # Envia o pacote para a leitura Gamma
+        try:
+
+            pct1 = incluirChecksum("\x07" + "01RM1")
+
+        #print(pct1)
+
+            serial_interface.write(pct1)
+
+        # Lê os oito caracteres enviados Gamma
+
+            dataGamma = serial_interface.read(50)
+
+        # Envia o pacote para a leitura Neutron
+
+            pct2 = incluirChecksum("\x07" + "01RM2")
+
+        #print(pct2)
+
+            serial_interface.write(pct2)
+
+        # Lê os oito caracteres enviados Neutron
+
+            dataNeutron = serial_interface.read(50)
+
+        # Calcula o valor da taxa de dose instantânea (em uSv/h)
+        # descobrir como chegam os dados e trata-los
+        # condicional caso a mensagem recebida for vazia por problema na sonda o algoritmo nao faz nada
+
+            if dataGamma != "" and dataNeutron != "":
+                dataG = float(dataGamma.split(" ")[1])
+                dataN = float(dataNeutron.split(" ")[1])
+
+            #print("Radiação Gamma:     " + "{}".format(dataG) + " uSv/h")
+            #print("Radiação Neutron:   " + "{}".format(dataN) + " uSv/h")
+
+        # Imprime na tela o resultado
+
+            dataTotal = dataG + dataN
+
+
+        #print("Radiação Total:     " + "{}".format(dataTotal) + " uSv/h")
+        #print("\n")
+
+        # Calcula o valor em Sv/h e ja que terei que multiplicar por segundo preciso de converter
+        # De uSv/h para Sv/s e multiplicar por delta T para obter a área
+
+            gammaBuffer.append(dataG)
+            neutronBuffer.append(dataN)
+            dataBuffer.append(dataTotal)
+
+        # Tendo em vista que a integral começa zerada a soma de todos os vetores é igual a zero logo não
+        # preciso somar todos os vetores.
+
+            timeBuffer.append(datetime.datetime.utcnow())
+
+            if timeBuffer[-1] != 0 and timeBuffer[-2] != 0:
+
+                if (timeBuffer[-1] - timeref).total_seconds() > sample and (timeBuffer[-2] - timeref).total_seconds() < sample:
+
+                    integralneutron = 0
+                    integralgamma = 0
+                    integral = 0
+                    j=0
+
+                    while j <= i:
+                        
+                        integralgamma += ((gammaBuffer[j] + gammaBuffer[j+1]) * deltatimeBuffer[j]) / (2 * 3600)
+                        integralneutron += ((neutronBuffer[j] + neutronBuffer[j + 1]) * deltatimeBuffer[j]) / (2 * 3600)
+                        integral += ((dataBuffer[j] + dataBuffer[j + 1]) * deltatimeBuffer[j]) / (2 * 3600)
+                        j += 1
+
+                    iterate = i
+                    timeref = timeBuffer[-1]
+                    i=0
+
+                else:
+
+                    deltatime = time_sec(timeBuffer)
+                    deltatimeBuffer.append(deltatime)
+                    integralgamma += ((((gammaBuffer[-1] + gammaBuffer[-2]) * deltatime) - ((gammaBuffer[0] + gammaBuffer[1]) * deltatimeBuffer[0])) / (2 * 3600))
+                    integralneutron += ((((neutronBuffer[-1] + neutronBuffer[-2]) * deltatime) - ((neutronBuffer[0] + neutronBuffer[1]) * deltatimeBuffer[0])) / (2 * 3600))
+                    integral += ((((dataBuffer[-1] + dataBuffer[-2]) * deltatime) - ((dataBuffer[0] + dataBuffer[1]) * deltatimeBuffer[0])) / (2*3600))
+
+        # Apaga o primeiro elemento do vetor ao adicionar um novo caso passe do tamanho sample
+
+            if len(dataBuffer) > iterate:
+                dataBuffer = dataBuffer[1:]
+
+            if len(timeBuffer) > 5:
+                timeBuffer = timeBuffer[1:]
+
+            if len(deltatimeBuffer) > iterate:
+                deltatimeBuffer = deltatimeBuffer[1:]
+
+            if len(gammaBuffer) > iterate:
+                gammaBuffer = gammaBuffer[1:]
+
+            if len(neutronBuffer) > iterate:
+                neutronBuffer = neutronBuffer[1:]
+
+            i += 1
+
+        # print("O valor da integral em 4h é:  " + "{}".format(integral) + " uSv")
+        # print("\n")
+
+        except Exception as e:
+            print(e)
+            logging.error("Error occurred" + str(e))
+            pass
+
+
+# aux a thread principal
+
+auxiliary_thread = threading.Thread(target = scanThread)
+auxiliary_thread.setDaemon(True)
+auxiliary_thread.start()
+
+# O programa em sua verção final irá dormir 500s para garantir a leitura certa da sonda
+
+udp_server_address = ("0.0.0.0", UDP_PORT)
+udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_server_socket.bind(udp_server_address)
+
+# Loop Principal
+
+while (True):
+
+    # Client (EPICS IOC) input data and address
+
+    data, address = udp_server_socket.recvfrom(512)
+
+    # There is a simple protocol for communication to the client
+
+    if (data):
+
+        if (data == "RAD_G?\n"):
+            answer = "{:.10f}".format(gammaBuffer[-1])
+
+        elif (data == "RAD_N?\n"):
+            answer = "{:.10f}".format(neutronBuffer[-1])
+
+        elif (data == "RAD_T?\n"):
+            answer = "{:.10f}".format(dataBuffer[-1])
+
+        elif (data == "RAD_I?\n"):
+            answer = "{:.10f}".format(integral)
+
+        elif (data == "RAD_I_G?\n"):
+            answer = "{:.10f}".format(integralgamma)
+
+        elif (data == "RAD_I_N?\n"):
+            answer = "{:.10f}".format(integralneutron)
+
+        else:
+            continue
+
+        answer += "\n"
+        udp_server_socket.sendto(answer, address)
+
+        # The end
