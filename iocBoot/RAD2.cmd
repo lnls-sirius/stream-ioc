6a994e9f9ba5fc757b7d79377af57e9b338ea745
Aureo Freitas <aureo.carneiro@gmail.com>
2018-07-18 17:01:25 -0300

    Updating Thermo-FHT6020 files for the new version of IOC

--- iocBoot/RAD2.cmd
+++ iocBoot/RAD2.cmd
@@ -7,7 +7,7 @@
 
 # Environment variables
 
-epicsEnvSet("EPICS_CAS_INTF_ADDR_LIST", "10.0.38.23") 
+#epicsEnvSet("EPICS_CAS_INTF_ADDR_LIST", "10.0.38.24") 
 
 epicsEnvSet("STREAM_IOC", "/root/stream-ioc")
 epicsEnvSet("EPICS_BASE", "/opt/epics-R3.15.5/base")
